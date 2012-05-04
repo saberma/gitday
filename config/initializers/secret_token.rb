f38efd2123904edd22376e22ebc434ccfb716da0
saberma <mahb45@gmail.com>
2012-05-04 23:28:49 +0800

    init rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubFriend::Application.config.secret_token = 'd678201458d875b6bc060ff7891ea8e2afc6e846ccca778d6f00d87e01764e6410d28c6421eca3d4660233046d3f537e2b56c1d468e26a5b01910df1fa6de827'
