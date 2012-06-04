69cd7829349cdc2861bcd0f177b8e8be5e1bc817
saberma <mahb45@gmail.com>
2012-06-04 10:33:17 +0800

    add email.css for precompile.

--- spec/mailers/subscriber_spec.rb
+++ spec/mailers/subscriber_spec.rb
@@ -1,18 +1,4 @@
 require "spec_helper"
 
 describe Subscriber do
-  describe "day" do
-    let(:mail) { Subscriber.day }
-
-    it "renders the headers" do
-      mail.subject.should eq("Day")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      mail.body.encoded.should match("Hi")
-    end
-  end
-
 end
