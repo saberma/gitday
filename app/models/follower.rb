9e5544a3dd9ca11c484f850ac58a4ca49ffdb219
saberma <mahb45@gmail.com>
2012-06-24 19:58:29 +0800

    fixed rspec error.

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,7 +1,7 @@
 class Follower < ActiveRecord::Base
   belongs_to :day
   belongs_to :author, class_name: 'User'
-  attr_accessible :author_id, :published_at
+  attr_accessible :author, :published_at
 
   module Extension
 
