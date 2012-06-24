a4b55f6e5476381973881d0328740d9138a8e40e
saberma <mahb45@gmail.com>
2012-06-24 11:50:54 +0800

    save member time zone.

--- app/controllers/members/token_controller.rb
+++ app/controllers/members/token_controller.rb
@@ -1,7 +1,7 @@
 class Members::TokenController < ApplicationController
 
   def update
-    current_member.update_attributes token: params[:member][:token]
+    current_member.update_attributes token: params[:member][:token], time_zone: params[:member][:time_zone]
     redirect_to root_path, notice: "Update success!"
   end
 
