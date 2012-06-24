5553b03d20231f9077d1e966a8ab1ea89b9a94a0
saberma <mahb45@gmail.com>
2012-06-24 20:33:01 +0800

    dev use letter_opener by default.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,16 +16,16 @@ Gitday::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: "lvh.me" }
-  #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'gitday.com',
-    :user_name            => SecretSetting.smtp.username,
-    :password             => SecretSetting.smtp.password,
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :domain               => 'gitday.com',
+  #  :user_name            => SecretSetting.smtp.username,
+  #  :password             => SecretSetting.smtp.password,
+  #  :authentication       => 'plain',
+  #  :enable_starttls_auto => true  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
