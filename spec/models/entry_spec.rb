db8a7aada922640b88aafc665cd344c395d45e39
saberma <mahb45@gmail.com>
2012-07-15 11:38:52 +0800

    refactor comment entry rspec test.

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -6,19 +6,19 @@ describe Entry do
 
   let(:day) { Factory(:day, member: member) }
 
+  let(:entry) { day.entries.first }
+
+  before do
+    day.entries.add feed_entry
+  end
+
   context 'comment' do
 
-    let(:entry) { Factory(:comment_on_jquery_file_upload_entry, day: day) }
+    let(:feed_entry) { parse("comment_event.xml").entries.first }
 
-    it 'should get repo' do
+    it 'should be create' do
       entry.active_repository.should eql 'blueimp/jQuery-File-Upload'
-    end
-
-    it 'should get the issue id' do
       entry.issue_number.should eql 1223
-    end
-
-    it 'should get the id' do
       entry.comment_id.should eql 6733474
     end
 
@@ -26,22 +26,12 @@ describe Entry do
 
   context 'push' do
 
-    context '#create' do
+    let(:feed_entry) { parse("push_event.xml").entries.first }
 
-      let(:feed_entry) { parse("push_event.xml").entries.first }
-
-      let(:entry) { day.entries.first }
-
-      before do
-        day.entries.add feed_entry
-      end
-
-      it 'should be success' do
-        entry.link.should eql 'elasticsearch/elasticsearch/compare/aafa8cc905...a5e541351f'
-        entry.ref.should eql '0.19'
-        entry.shas.should eql ['a5e5413']
-      end
-
+    it 'should be create' do
+      entry.link.should eql 'elasticsearch/elasticsearch/compare/aafa8cc905...a5e541351f'
+      entry.ref.should eql '0.19'
+      entry.shas.should eql ['a5e5413']
     end
 
   end
